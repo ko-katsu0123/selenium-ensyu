3cf45a06c625f9bf236641edd5477a313d862fb7
koichirokatsuyama <koichiro.katsuyama@rakus-partners.co.jp>
2021-05-21 10:03:11 +0900

    環境構築・動作確認済み

---
+++ src/ensyu/ensyu0.java
@@ -0,0 +1,25 @@
+package ensyu;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ensyu0 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./exe/chromedriver.exe");
+		WebDriver driver = new ChromeDriver(); //ChromeDriverの作成
+		driver.get("http://www.google.com/xhtml");	//get()でブラウザ起動
+		Thread.sleep(5000); //処理を停止(動作確認の為)
+	
+		//ブラウザの要素を指定　メソッド名がわかりやすいですよね。
+		WebElement	searchBox	=	driver.findElement(By.name("q"));
+		searchBox.sendKeys("ChromeDriver");
+		searchBox.submit();
+		Thread.sleep(5000);		
+		driver.quit();	//ブラウザを閉じる
+
+	}
+
+}
